8b7b5a55e4b2e727f8330d6a35f1933b9725e4f0
Simon Lind <simon@purplemesh.net>
2013-10-14 18:33:36 +0300

    Monday night status quo
    all modals working.
    
    Missing: Form validation, contact page, modal colors and buttons.

--- orderform.php
+++ orderform.php
@@ -5,6 +5,14 @@ $company = $_POST['company'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 $order = $_POST['order'];
+if(isset($_POST['order_description']))
+{
+	$description = $_POST['order_description'];
+}
+else
+{
+	$description = 'none';
+}
 
 if($_POST['subscribe'] === 'subscribe')
 {
@@ -18,9 +26,16 @@ else
 
 $to = 'simon@purplemesh.net';
 $subject = 'Order request from website';
-$message = 'FROM: '.$name.'<br> Company: '.$company.'<br>Email: '.$email.'<br>Phone: '.$phone.'<br>Order: '.$order.'<br>Subscribe: '.$subscribe;
 $headers = 'From: youremail@domain.com';
 
+if($description == 'none')
+{
+$message = 'FROM: '.$name.'<br> Company: '.$company.'<br>Email: '.$email.'<br>Phone: '.$phone.'<br>Order: '.$order.'<br>Subscribe: '.$subscribe;
+}
+else
+{
+$message = 'FROM: '.$name.'<br> Company: '.$company.'<br>Email: '.$email.'<br>Phone: '.$phone.'<br>Order: '.$order.'<br>Subscribe: '.$subscribe.'<br>Description: '.$description;
+}
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
 mail($to, $subject, $message) or die('Error sending Mail'); //This method sends the mail.
 echo "Your email was sent!"; // success message
